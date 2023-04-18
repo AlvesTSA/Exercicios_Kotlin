11efe4aa27227c758a4ace9ef4e9fce6b4a28dde
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-18 14:40:59 -0300

    exercicio_18

---
+++ Estrutura_Sequencial/exercicio_018.kt
@@ -0,0 +1,20 @@
+/*Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos). */
+
+fun main(){
+
+    val mb: Double
+    val mbps: Double
+    val t: Double
+
+    println("Informe o tamanho do arquivo em MB: ");
+    mb = readLine()!!.toDouble()
+    
+    println("Informe a velocidade em Mbps: ");
+    mbps = readLine()!!.toDouble()
+
+    t = mb/mbps
+
+    println("O tempo para dowloading: $t min")
+
+
+}
